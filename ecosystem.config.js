00daaf497e64184bab61343aaef2d6683a5f2daf
Frank Yan <frank.yan@italki.com>
2022-06-16 14:20:55 +0800

    ci: 🎡 init pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      script: "./public/server.app.js",
+      args: "one two",
+      instances: 2,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "root",
+      host: "150.158.46.8",
+      ref: "origin/master",
+      repo: "git@github.com:mbaxszy7/music-motion-x.git",
+      path: "/root/music-motion-x/production",
+      "post-deploy":
+        "git pull && sudo npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+    },
+  },
+}
