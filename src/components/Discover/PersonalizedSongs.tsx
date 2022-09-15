a49bcaa8621af046d2267640eddec2cb47b76108
Frank Yan <frank.yan@italki.com>
2022-09-15 22:46:26 +0800

    test image load

--- src/components/Discover/PersonalizedSongs.tsx
+++ src/components/Discover/PersonalizedSongs.tsx
@@ -23,17 +23,9 @@ const PersonalizedSongs = memo(() => {
       >
         {threePersonalizedSongs?.map((pic, index) =>
           index === 2 ? (
-            <MyImage
-              key={index}
-              url={pic ? `${pic}?param=67y67` : ""}
-              className={css.center_image}
-            />
+            <MyImage key={index} url={""} className={css.center_image} />
           ) : (
-            <MyImage
-              key={index}
-              url={pic ? `${pic}?param=76y76` : ""}
-              className=""
-            />
+            <MyImage key={index} url={""} className="" />
           ),
         )}
       </div>
